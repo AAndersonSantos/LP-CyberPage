68e80b385bfc2c98abd30737f99a6bcb13f9f939
AAndersonSantos <andersonsantos.inc01@gmail.com>
2023-03-06 17:17:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 0,
+    autoplay:true,
+    autoplayTimeout:6000,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active-Collapse");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 30;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } 
+  }
+}
+
+window.addEventListener("scroll", reveal);
